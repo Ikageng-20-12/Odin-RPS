56b3f163fb15fbe296f34606b73c94771cf52ef1
Ikageng-20-12 <fthitane@gmail.com>
2024-10-13 15:24:03 +0200

    Added front end for the game

--- gamelogic.js
+++ gamelogic.js
@@ -1,6 +1,8 @@
 
 let options = ["rock", "paper", "scissors"];
-
+let random_index = Math.floor(Math.random() * 8);
+let random_comments_comp = ["How are you so bad at this? Are you For Real?", "I'm sorry, I'm just too good at this", "You're not very good at this, are you?","I cannot believe you are being destroyed by a Computer, you supposed to be my maker"]
+let random_comments_user = ["You're on fire!", "You're a genius!", "You're a legend!", "You're a beast!", "I guess you are really my maker, Just Better!"];
 let userchoice = prompt("Please enter your choice (rock, paper, or scissors): ");
 const computer_choice = () => {
     let random = Math.floor(Math.random() * 3);
@@ -11,13 +13,15 @@ const computer_choice = () => {
 function logic(choice){
 
     if (choice === computer_choice()) {
-        console.log("Would you look at that, great minds think alike! It's a tie!");
+        console.log("It's a tie!, Great minds think alike!");
     }
     else if(choice === "rock" && computer_choice() === "scissors" || choice === "scissors" && computer_choice() === "paper" || choice === "paper" && computer_choice() === "rock"){
-        console.log("Congratulations! You won!");
+        let comment = random_comments_user[random_index];
+        console.log(comment);
     }
     else if(choice === "rock" && computer_choice() === "paper" || choice === "scissors" && computer_choice() === "rock" || choice === "paper" && computer_choice() === "scissors"){
-        console.log("Sorry, you lost!", "I as the computer went for: ", computer_choice());
+        let comment = random_comments_comp[random_index];
+        console.log(comment);
     }
     else{
         console.log("Invalid input. Please enter rock, paper, or scissors!!!");
